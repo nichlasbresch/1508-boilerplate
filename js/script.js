5990977acf3bc0096155cda4e48acdfd75d2b8e3
Henrik Kok <kok@me.com>
2011-09-13 10:07:05 +0200

    First commit, with the boilerplate

---
+++ js/script.js
@@ -0,0 +1,14 @@
+(function($){ //Anonomous namespace
+
+document.body.className = document.body.className.replace('no-js', ''); //Remove no-js
+
+$(function(){ //jQuery Document Ready
+	
+	/* 
+	* Black magic goes here
+	*
+	-----------------------*/
+
+});
+
+})( jQuery )
