2a9698a1f332b3d25cd36b14ca246a3c544a0a16
Henrik Kok <kok@me.com>
2011-12-14 12:57:16 +0100

    Added new lightweight js functionality and less-1.1.5.min.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-document.body.className = document.body.className.replace('no-js', ''); //Remove no-js
 (function($){ //Anonomous namespace
 
 $(function(){ //jQuery Document Ready
-	
 	/* Black magic goes here */
 
 });
-
-})( jQuery )
+})( jQuery );
